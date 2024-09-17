3c89024cb68ce3ddad918a36324ee5bda94ff932
sohelhussain <sohelhussaing@gmail.com>
2024-09-17 12:33:18 +0530

    feat: WRITIMNG SOMTIHNG

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>Hello World</h1>
+      <p>This is a React Class Component</p>
+      <br />
+    </div>
   )
 }
 
