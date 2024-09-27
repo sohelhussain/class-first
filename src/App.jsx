f6e8db8a8bfc8f4535556494df4c10abe840b6c9
sohelhussain <sohelhussaing@gmail.com>
2024-09-27 17:26:54 +0530

    feat: RADIO BTN

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ function App() {
   }
 
   const [error, setError] = useState(true);
+  const [gender, setGender] = useState('other');
 
   return (
     <div className='w-screen h-screen bg-gray-100'>
       <form>
         <input onInput={e => e.target.value.length < 5 ? setError(true) : setError(false)} onChange={e => setValue(e.target.value)} name='name' type='text' placeholder='Enter your name' value={value} />
-        {error && <p>Please enter your name</p>}
+        {error && <p className='text-red-400'>Please enter your name</p>}
+
+        <input type="radio" name="other" defaultValue='other' checked={gender === 'other' ? true : false} onChange={e => setGender(e.target.value)}/>
+        <input type="radio" name="female" defaultValue='female' checked={gender === 'female' ? true : false} onChange={e => setGender(e.target.value)}/>
+        <input type="radio" name="male" defaultValue='male' checked={gender === 'male' ? true : false} onChange={e => setGender(e.target.value)}/>
+
+
         <button onClick={formHndler}>Submit</button>
       </form>
     </div>
