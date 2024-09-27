5323b35b39c1b2ebc4393bb1a66388ddf9e322d1
sohelhussain <sohelhussaing@gmail.com>
2024-09-27 16:40:22 +0530

    feat: CREATEING A LIVE TIMER

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+
+  const x = [{name:'sohel hussain'}]
+
+  const y = x.map((item, index)=> {
+    return item
+  })
+  // console.log(y);
+
+
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  const handleClick = () => {
+    console.log(time);
+  }
+  
+  setInterval(e => {
+    setTime(new Date().toLocaleTimeString())
+  },1000)
+
   return (
     <div>
       <h1>Hello World</h1>
       <p>This is a React Class Component</p>
+      {/* <h1>{x.map(item => item)}</h1> */}
+
+      <h2>{time}</h2>
+      <button onClick={handleClick}>click</button>
       <br />
     </div>
   )
